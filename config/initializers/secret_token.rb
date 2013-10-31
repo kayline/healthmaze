c9353b8f28680a9ebbbb7eebc2652c03d337eca4
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-10-30 18:55:54 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Healthmaze::Application.config.secret_key_base = '1ffe1f363cc76c056347b055808cbd1841b15240d7d6926286529771a4f0b944bc4ea01bfa2cd26cbea445a2946e250c72294640102ac0d9d2bef2a85625f0a2'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Healthmaze::Application.config.session_store :cookie_store, key: '_healthmaze_session'
