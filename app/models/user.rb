1e01b533b84e20d22e4f6ad29a1e2a490f8082c6
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-11-27 18:20:58 -0800

    create methods to reset user needs and plan costs

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,17 @@
 class User < ActiveRecord::Base
 	has_many :plans_users
 	has_many :needs
+
+	def reset_costs
+		self.plans_users.each do |pu|
+			pu.total_cost = 0
+			pu.care_costs = 0
+			pu.save
+		end
+	end
+
+	def reset_needs
+		self.needs = []
+		self.save
+	end
 end
