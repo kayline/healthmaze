909592b36e8494227755bdb87281758ad72aff47
Molly Trombley-McCann <molly.kayline@gmail.com>
2013-11-27 18:20:12 -0800

    fucking git won't let me split this hunk so this is the total get needs/set_needs pair of controller actions, plus strong params for need

--- app/controllers/maze_controller.rb
+++ app/controllers/maze_controller.rb
@@ -17,4 +17,29 @@ class MazeController < ApplicationController
 		@cares = Care.all
 		render 'needs'
 	end
+
+	def set_needs
+		current_user.reset_needs
+		current_user.reset_costs
+		@need = Need.new(need_params)
+		@need.user = current_user
+		if @need.save
+			@plan_results = current_user.plans_users
+			@plan_results.each do |pr|
+				puts "*******Calculating total cost for plan #{pr.plan.id}"
+				pr.calc_total_cost
+				puts "*******Total cost is #{pr.total_cost}**********"
+			end
+			redirect_to plans_path
+		else
+			@errors = @need.errors.full_messages
+			@cares = Care.all
+			render 'needs'
+		end
+	end
+
+	private
+	def need_params
+		params.require(:need).permit(:description, :frequency, :cost, :care_id)
+	end
 end
